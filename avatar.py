806478844c0969d1cf900998d7d630b8904ef18d
Nikodemus Karlsson <85926678+nika-edu@users.noreply.github.com>
2022-10-21 12:21:36 +0200

    Ändrade variabelnamn till engelska

--- avatar.py
+++ avatar.py
@@ -1,8 +1,8 @@
 while True:
-    styrka = input("Ange avatarens styrka som ett tal 1 - 10 -> ")
-    if styrka.isdigit():
-        styrka = int(styrka)
-        if styrka < 1 or styrka > 10:
+    strength = input("Ange avatarens styrka som ett tal 1 - 10 -> ")
+    if strength.isdigit():
+        strength = int(strength)
+        if strength < 1 or strength > 10:
             continue
         else:
             break
